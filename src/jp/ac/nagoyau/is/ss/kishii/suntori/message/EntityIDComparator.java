846ca22a9639169a08140bd3128a57fcaadad9b9
Anton Amlinger <antam167@student.liu.se>
2014-12-08 19:29:58 +0100

    Made comperator public to more easily extend the Converter.

--- src/jp/ac/nagoyau/is/ss/kishii/suntori/message/EntityIDComparator.java
+++ src/jp/ac/nagoyau/is/ss/kishii/suntori/message/EntityIDComparator.java
@@ -10,7 +10,7 @@ import rescuecore2.worldmodel.EntityID;
  * @author takefumi
  * 
  */
-class EntityIDComparator implements Comparator<EntityID> {
+public class EntityIDComparator implements Comparator<EntityID> {
 	@Override
 	public int compare(EntityID o1, EntityID o2) {
 		return o1.getValue() - o2.getValue();
