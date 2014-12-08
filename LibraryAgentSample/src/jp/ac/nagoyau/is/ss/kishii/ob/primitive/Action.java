6e341948d58a873e27fab4d3b895105164a35271
Anton Amlinger <antam167@student.liu.se>
2014-12-08 19:13:15 +0100

    Removed Sampleagent, not relevant for project.

--- LibraryAgentSample/src/jp/ac/nagoyau/is/ss/kishii/ob/primitive/Action.java
+++
@@ -1,5 +0,0 @@
-package jp.ac.nagoyau.is.ss.kishii.ob.primitive;
-
-public enum Action {
-	MOVE, REST, CLEAR, EXTINGUISH, RESCUE, LOAD, UNLOAD;
-}
--- LibraryAgentSample/src/jp/ac/nagoyau/is/ss/kishii/ob/primitive/svm/SvmFeatureData.java
+++
@@ -1,9 +0,0 @@
-package jp.ac.nagoyau.is.ss.kishii.ob.primitive.svm;
-
-import libsvm.wrapper.SvmFeatureVector;
-
-public class SvmFeatureData<K> extends FeatureData<K, SvmFeatureVector> {
-	public SvmFeatureData(K id, SvmFeatureVector feature) {
-		super(id, feature);
-	}
-}
